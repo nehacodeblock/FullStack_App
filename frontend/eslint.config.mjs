da470d95b4c7a1fa22cc4fa53d995f2e31d53d1c
nehacodeblock <nehaagarwal@codeblockonline.com>
2025-12-15 12:03:01 +0530

    test lint fail

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,14 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  {
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": "error",
+      "no-console": "error", // optional: blocks console.log
+    },
+  },
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
